26a37c8e1ae9c1414f705b803316211d19516f90
Drew Dahlquist <gypysmoth@gmail.com>
2022-01-07 22:38:47 -0600

    Some basic backend and db seutp.

---
+++ server/routes/experiment.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const { v4: uuidv4 } = require('uuid')
+
+// experimentRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const experimentRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This will help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+
+// This section will help you get a list of all the records.
+experimentRoutes.route("/experiment").get(function (req, res) {
+  let db_connect = dbo.getDb("dmc-db");
+  db_connect
+    .collection("experiments")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single record by id
+experimentRoutes.route("/experiment/:id").get(function (req, res) {
+  let db_connect = dbo.getDb("dmc-db");
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect
+    .collection("experiments")
+    .findOne(myquery, function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you create a new record.
+experimentRoutes.route("/experiment/add").post(function (req, response) {
+  let db_connect = dbo.getDb("dmc-db");
+  let myobj = {
+    // uuid: uuidv4(), // will make decision about this later
+    machine: req.body.machine,
+    experimentName: req.body.experimentName,
+    positions: req.body.positions,
+    frequency: req.body.frequency,
+    begDate: null,
+    endDate: req.body.endDate,
+    confirmation: req.body.confirmation,
+  };
+  db_connect.collection("experiments").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update a record by id.
+experimentRoutes.route("/experiment/update/:id").post(function (req, response) {
+  let db_connect = dbo.getDb("dmc-db");
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      machine: req.body.machine,
+      experimentName: req.body.experimentName,
+      positions: req.body.positions,
+      frequency: req.body.frequency,
+      begDate: null,
+      endDate: req.body.endDate,
+      confirmation: req.body.confirmation,
+    },
+  };
+  db_connect
+    .collection("experiment")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+// This section will help you delete a record
+// experimentRoutes.route("/:id").delete((req, response) => {
+//   let db_connect = dbo.getDb("dmc-db");
+//   let myquery = { _id: ObjectId(req.params.id) };
+//   db_connect.collection("records").deleteOne(myquery, function (err, obj) {
+//     if (err) throw err;
+//     console.log("1 document deleted");
+//     response.status(obj);
+//   });
+// });
+
+module.exports = experimentRoutes;
